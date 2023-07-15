fd30925d2befd7c03c4349e04d708e1eb94af45d
Arwin-S <arwin.sondhi@mail.utoronto.ca>
2023-07-14 22:30:43 -0400

    Integrated Twilio api to send sms notficiation

--- scraper.py
+++ scraper.py
@@ -3,65 +3,84 @@ from selenium.webdriver.common.by import By
 
 from bs4 import BeautifulSoup
 import time
-# Set up Selenium WebDriver (you may need to specify the path to your WebDriver executable)
-driver = webdriver.Chrome()
-
-# Load the initial page
-driver.get("https://www.cineplex.com/theatre/scotiabank-theatre-toronto")
-time.sleep(2)
-
-# Find the button using text
-driver.find_element(By.XPATH, '//button[normalize-space()="Get Tickets"]').click()
-
-# Wait for the side pane to load (adjust the wait time as needed)
-time.sleep(2)
-
-# Find the button using text
-driver.find_element(By.XPATH, '//button[@type="button" and @data-name="select-Date"]').click()
-time.sleep(2)
-
-# driver.find_element(By.TAG_NAME, 'h4')
-# time.sleep(5)
-
-# Get the updated page source after the button click
-updated_page_source = driver.page_source
-
-# Close the Selenium WebDriver
-driver.quit()
-
-# Parse the updated page source using BeautifulSoup
-soup = BeautifulSoup(updated_page_source, "html.parser")
+from twilio.rest import Client
+import getpass
 
 
-# Find the element you want to monitor for changes
-elements = soup.find_all('h4')
-if elements:
-    for element in elements:
-        print(element.text.strip())
-else:
-    print("Element not found.")
+def web_scraper(day) -> int:
+    # Set up Selenium WebDriver (you may need to specify the path to your WebDriver executable)
+    driver = webdriver.Chrome()
+
+    # Load the initial page
+    driver.get("https://www.cineplex.com/theatre/scotiabank-theatre-toronto")
+    time.sleep(5)
+
+    # Find the button using text
+    driver.find_element(By.XPATH, '//button[normalize-space()="Get Tickets"]').click()
+    time.sleep(8)
+
+    # Find the button using text
+    driver.find_element(By.XPATH, '//button[@type="button" and @data-name="select-Date"]').click()
+    time.sleep(5)
+
+    # driver.find_element(By.TAG_NAME, 'h4')
+    # time.sleep(5)
+
+    # Get the updated page source after the button click
+    updated_page_source = driver.page_source
+
+    # Close the Selenium WebDriver
+    driver.quit()
+
+    # Parse the updated page source using BeautifulSoup
+    soup = BeautifulSoup(updated_page_source, "html.parser")
+
+
+    # Find the element you want to monitor for changes
+    elements = soup.find_all('h4')
+
+    if elements:
+        return len(list(filter(lambda d: d.text.strip() == day, elements)))
+    else:
+        return 0 # No days found (scraper broken)
+    
+def notify(status, client):
+    if status == True:
+        print('Tickets are available')
+        message = client.messages.create(
+            body='TICKETS AVAILABLE',
+            from_=from_phone_num,
+            to=to_phone_num
+        )
+    else:
+        print('Web Scraper Broken')
+        message = client.messages.create(
+            body='SCRAPER BROKEN',
+            from_=from_phone_num,
+            to=to_phone_num
+        )
+    print(message.sid)
     
     
-# # Save the initial content of the element
-# initial_content = element.text.strip()
-
-# # Function to check for changes in the element
-# def check_for_changes():
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.content, "html.parser")
-#     updated_element = soup.find("h1", class_="header")
-#     updated_content = updated_element.text.strip()
+if __name__ == '__main__':
     
-#     if updated_content != initial_content:
-#         print("The element has changed!")
-#         print("Old content:", initial_content)
-#         print("New content:", updated_content)
-#         # Add your notification mechanism here
-#     else:
-#         print("No changes detected.")
+    account_sid = getpass.getpass("Account SID:")
+    auth_token = getpass.getpass("Auth Token:")
+    to_phone_num = getpass.getpass("Phone Number to Notify:")
+    from_phone_num = getpass.getpass("Phone Number to Send From:")
+    
 
-# # Run the change detection function periodically
-# # You can customize the frequency and duration as per your needs
-# while True:
-#     check_for_changes()
-#     time.sleep(60)  # Wait for 60 seconds before checking again
+    
+    client = Client(account_sid, auth_token)
+    
+    day = 'Saturday'
+    # day = 'Wednesday'
+    while(True):
+        numDays = web_scraper(day)
+        if numDays == 0:
+            notify(False, client)
+        elif numDays > 1:
+            notify(True, client)
+        else:
+        
+        time.sleep(10)
